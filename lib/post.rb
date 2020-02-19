5b5341e59008588b844d2f46491bca562a4b0476
Hailey Bongo <haileyebongo@gmail.com>
2020-02-19 20:48:33 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,35 @@
+class Post 
+  attr_accessor :title, :author  
+  
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    save
+  end
+ 
+  def save
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def name 
+    @title 
+  end
+  
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil 
+    end
+  end
+    
+  
+
+end
+    
+    
